3659fd683c4b5ec9383d55521c47cacee8441554
Tony Glover <tony@drt-software.com>
2024-05-22 11:56:53 +0100

    Calculate venus angle

--- src/Scene/Planets/Venus.tsx
+++ src/Scene/Planets/Venus.tsx
@@ -1,3 +1,4 @@
+import { Vector3 } from "three";
 import { Sphere, useTexture } from "@react-three/drei";
 import { PLANETS } from "../../state/Config";
 import Path from "../Path"
@@ -5,9 +6,13 @@ import Path from "../Path"
 const Venus = () => {
   const atmosphere = useTexture("./textures/venus_atmosphere.jpg");
 
+  // Calculate planet position
+  const distance = new Vector3(PLANETS.VENUS.distance, 0, 0);
+  const position = distance.applyAxisAngle(new Vector3(0, 1, 0), PLANETS.VENUS.angle);
+
   return (
     <>
-    <Sphere position-x={PLANETS.VENUS.distance} scale={PLANETS.VENUS.radius}>
+    <Sphere position={position} scale={PLANETS.VENUS.radius}>
       <meshStandardMaterial map={atmosphere} />
     </Sphere>
     <Path startDistance={PLANETS.VENUS.distance}/>
