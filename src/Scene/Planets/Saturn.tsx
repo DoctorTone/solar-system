370098ee6f8538f375953dfd3b37572d4294346e
DoctorTone <tony@drt-software.com>
2024-05-22 02:01:09 +0100

    Add saturn's rings

--- src/Scene/Planets/Saturn.tsx
+++ src/Scene/Planets/Saturn.tsx
@@ -1,13 +1,27 @@
-import { Sphere, useTexture } from "@react-three/drei";
+import { DoubleSide } from "three";
+import { Sphere, useTexture, Ring } from "@react-three/drei";
 import { PLANETS } from "../../state/Config";
 
 const Saturn = () => {
   const surface = useTexture("./textures/saturn.jpg");
+  const rings = useTexture("./textures/saturn_ring.png");
 
   return (
-    <Sphere position-x={PLANETS.SATURN.distance} scale={PLANETS.SATURN.radius}>
-      <meshStandardMaterial map={surface} />
-    </Sphere>
+    <>
+      <Sphere
+        position-x={PLANETS.SATURN.distance}
+        scale={PLANETS.SATURN.radius}
+      >
+        <meshStandardMaterial map={surface} />
+      </Sphere>
+      <Ring
+        args={[PLANETS.SATURN.radius + 2, PLANETS.SATURN.radius + 10, 32]}
+        position-x={PLANETS.SATURN.distance}
+        rotation-x={Math.PI / 2}
+      >
+        <meshStandardMaterial side={DoubleSide} map={rings} />
+      </Ring>
+    </>
   );
 };
 
