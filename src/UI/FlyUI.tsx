e7e45f737922393a3913a1234b08078f305fb50d
Tony Glover <tony@drt-software.com>
2024-05-25 16:27:22 +0100

    Use speed dial for fly to

--- src/UI/FlyUI.tsx
+++ src/UI/FlyUI.tsx
@@ -1,82 +1,40 @@
-import { useState, ChangeEvent } from "react";
-import Fab from "@mui/material/Fab";
-import NavigationIcon from "@mui/icons-material/Navigation";
-import Radio from "@mui/material/Radio";
-import RadioGroup from "@mui/material/RadioGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import useStore from "../state/store";
-import { blue, grey } from "@mui/material/colors";
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import PrintIcon from '@mui/icons-material/Print';
+import ShareIcon from '@mui/icons-material/Share';
+import Typography from '@mui/material/Typography';
+
+const actions = [
+  { icon: <FileCopyIcon />, name: 'Copy' },
+  { icon: <SaveIcon />, name: 'Save' },
+  { icon: <PrintIcon />, name: 'Print' },
+  { icon: <ShareIcon />, name: 'Share' },
+];
 
 const FlyUI = () => {
-  const [showList, setShowList] = useState(false);
-  const setCurrentView = useStore((state) => state.setCurrentView);
-  const currentView = useStore((state) => state.currentView);
-
-  const toggleList = () => {
-    setShowList(!showList);
-  };
-
-  const changeView = (event: ChangeEvent<HTMLInputElement>, value: string) => {
-    setShowList(false);
-    setCurrentView(value);
-  };
-
   return (
     <div id="flyUI" className="panel">
-      <Fab
-        sx={{
-          mb: 3,
-          backgroundColor: blue[400],
-          "&:hover": {
-            backgroundColor: blue[400],
-          },
-        }}
-        onClick={toggleList}
-        size="small"
-        variant="extended"
+      <Typography variant="button" display="block" gutterBottom>
+{" "}Fly To :
+      </Typography>
+      <SpeedDial
+        ariaLabel="SpeedDial basic example"
+        direction='down'
+        icon={<SpeedDialIcon />}
       >
-        <NavigationIcon sx={{ mr: 1 }} />
-        Fly To
-      </Fab>
-      {showList && (
-        <RadioGroup
-          sx={{
-            backgroundColor: grey[900],
-            opacity: 0.75,
-            borderRadius: "5px",
-            padding: "1% 1% 1% 10%",
-          }}
-          aria-labelledby="demo-radio-buttons-group-label"
-          defaultValue="start"
-          name="radio-buttons-group"
-          value={currentView}
-          onChange={changeView}
-        >
-          <FormControlLabel value="start" control={<Radio />} label="Start" />
-          <FormControlLabel
-            value="mercury"
-            control={<Radio />}
-            label="Mercury"
+        {actions.map((action) => (
+          <SpeedDialAction
+            key={action.name}
+            icon={action.icon}
+            tooltipTitle={action.name}
           />
-          <FormControlLabel value="venus" control={<Radio />} label="Venus" />
-          <FormControlLabel value="earth" control={<Radio />} label="Earth" />
-          <FormControlLabel value="mars" control={<Radio />} label="Mars" />
-          <FormControlLabel
-            value="jupiter"
-            control={<Radio />}
-            label="Jupiter"
-          />
-          <FormControlLabel value="saturn" control={<Radio />} label="Saturn" />
-          <FormControlLabel value="uranus" control={<Radio />} label="Uranus" />
-          <FormControlLabel
-            value="neptune"
-            control={<Radio />}
-            label="Neptune"
-          />
-        </RadioGroup>
-      )}
-    </div>
+        ))}
+      </SpeedDial>
+      </div>
   );
-};
+}
 
-export default FlyUI;
+export default FlyUI
