5c32505322efccc441bf34d23c565c6f6ef88508
DoctorTone <tony@drt-software.com>
2024-05-29 17:24:21 +0100

    Added all fly ui button images

--- src/UI/FlyUI.tsx
+++ src/UI/FlyUI.tsx
@@ -1,22 +1,26 @@
-import SpeedDial from '@mui/material/SpeedDial';
-import SpeedDialAction from '@mui/material/SpeedDialAction';
-import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
-import LanguageIcon from '@mui/icons-material/Language';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import Typography from '@mui/material/Typography';
-import { grey } from '@mui/material/colors';
+import SpeedDial from "@mui/material/SpeedDial";
+import SpeedDialAction from "@mui/material/SpeedDialAction";
+import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
+import LanguageIcon from "@mui/icons-material/Language";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import Typography from "@mui/material/Typography";
+import { grey } from "@mui/material/colors";
 import useStore from "../state/store";
 
 const actions = [
-  { icon: <HomeOutlinedIcon />, planet: 'Start' },
-  { icon: <LanguageIcon />, planet: 'Mercury' },
-  { icon: <LanguageIcon />, planet: 'Venus' },
-  { icon: <LanguageIcon />, planet: 'Earth' },
-  { icon: <LanguageIcon />, planet: 'Mars' },
-  { icon: <LanguageIcon />, planet: 'Jupiter' },
-  { icon: <LanguageIcon />, planet: 'Saturn' },
-  { icon: <LanguageIcon />, planet: 'Uranus' },
-  { icon: <LanguageIcon />, planet: 'Neptune' },
+  {
+    icon: <HomeOutlinedIcon />,
+    planet: "Start",
+    image: "",
+  },
+  { icon: <LanguageIcon />, planet: "Mercury", image: "./images/mercury.webp" },
+  { icon: <LanguageIcon />, planet: "Venus", image: "./images/venus.webp" },
+  { icon: <LanguageIcon />, planet: "Earth", image: "./images/earth.jpg" },
+  { icon: <LanguageIcon />, planet: "Mars", image: "./images/mars.jpg" },
+  { icon: <LanguageIcon />, planet: "Jupiter", image: "./images/jupiter.webp" },
+  { icon: <LanguageIcon />, planet: "Saturn", image: "./images/saturn.webp" },
+  { icon: <LanguageIcon />, planet: "Uranus", image: "./images/uranus.png" },
+  { icon: <LanguageIcon />, planet: "Neptune", image: "./images/neptune.png" },
 ];
 
 const FlyUI = () => {
@@ -24,35 +28,40 @@ const FlyUI = () => {
 
   const selectPlanet = (planet: string) => {
     const planetName = planet.toLowerCase();
-setCurrentView(planetName);
-  }
+    setCurrentView(planetName);
+  };
 
   return (
     <div id="flyUI" className="panel">
       <Typography variant="button" display="block" gutterBottom>
-{" "}Fly To :
+        {" "}
+        Fly To :
       </Typography>
       <SpeedDial
         ariaLabel="SpeedDial basic example"
-        direction='down'
+        direction="down"
         icon={<RocketLaunchIcon />}
-        FabProps={{color: "primary"}}
+        FabProps={{ color: "primary" }}
       >
         {actions.map((action) => (
           <SpeedDialAction
-          onClick={(event) => {
-            selectPlanet(action.planet);
-          }}
+            onClick={(event) => {
+              selectPlanet(action.planet);
+            }}
             key={action.planet}
             tooltipTitle={action.planet}
-            icon={action.icon}
             tooltipPlacement="right"
-            sx={{color: "orange", backgroundColor: grey[600]}}
+            sx={{
+              backgroundImage: `url(${action.image})`,
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
           />
         ))}
       </SpeedDial>
-      </div>
+    </div>
   );
-}
+};
 
-export default FlyUI
+export default FlyUI;
