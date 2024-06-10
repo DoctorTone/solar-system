fb4caf9ec38d149eab2e37e3d00d54f7a24e2a4d
DoctorTone <tony@drt-software.com>
2024-06-10 18:07:56 +0100

    Set up sun material shader

--- src/Scene/Planets/Sun.tsx
+++ src/Scene/Planets/Sun.tsx
@@ -1,23 +1,30 @@
-import { Sphere, useTexture } from "@react-three/drei";
+import { Sphere, useTexture, shaderMaterial } from "@react-three/drei";
+import { extend } from "@react-three/fiber";
 import { SUN } from "../../state/Config";
 import { SCENE } from "../../state/Config";
+import sunVertexShader from "../../shaders/sunVertexShader.glsl";
+import sunFragmentShader from "../../shaders/sunFragmentShader.glsl";
 
 const Sun = () => {
   const sunSurface = useTexture("./textures/sun.jpg");
   const sunBloom = useTexture("./textures/moonBloom.png");
+  const SunMaterial = shaderMaterial({}, sunVertexShader, sunFragmentShader);
+  extend({ SunMaterial });
 
   return (
     <>
-      <Sphere
-        args={[
-          SCENE.PLANET_RADIUS,
-          SCENE.PLANET_WIDTH_SEGMENTS,
-          SCENE.PLANET_HEIGHT_SEGMENTS,
-        ]}
-        scale={SUN.radius}
-      >
-        <meshStandardMaterial color={"yellow"} map={sunSurface} />
-      </Sphere>
+      <group scale={SUN.radius}>
+        <mesh>
+          <sphereGeometry
+            args={[
+              SCENE.PLANET_RADIUS,
+              SCENE.PLANET_WIDTH_SEGMENTS,
+              SCENE.PLANET_HEIGHT_SEGMENTS,
+            ]}
+          />
+          <sunMaterial />
+        </mesh>
+      </group>
       <group scale={SUN.radius * 3}>
         <sprite>
           <spriteMaterial attach="material" map={sunBloom} />
