e59882fdc79055de5c77095824992f894358902f
DoctorTone <tony@drt-software.com>
2024-06-10 16:37:39 +0100

    Add sun bloom texture

--- src/Scene/Planets/Sun.tsx
+++ src/Scene/Planets/Sun.tsx
@@ -4,18 +4,26 @@ import { SCENE } from "../../state/Config";
 
 const Sun = () => {
   const sunSurface = useTexture("./textures/sun.jpg");
+  const sunBloom = useTexture("./textures/moonBloom.png");
 
   return (
-    <Sphere
-      args={[
-        SCENE.PLANET_RADIUS,
-        SCENE.PLANET_WIDTH_SEGMENTS,
-        SCENE.PLANET_HEIGHT_SEGMENTS,
-      ]}
-      scale={SUN.radius}
-    >
-      <meshStandardMaterial color={"yellow"} map={sunSurface} />
-    </Sphere>
+    <>
+      <Sphere
+        args={[
+          SCENE.PLANET_RADIUS,
+          SCENE.PLANET_WIDTH_SEGMENTS,
+          SCENE.PLANET_HEIGHT_SEGMENTS,
+        ]}
+        scale={SUN.radius}
+      >
+        <meshStandardMaterial color={"yellow"} map={sunSurface} />
+      </Sphere>
+      <group scale={SUN.radius * 3}>
+        <sprite>
+          <spriteMaterial attach="material" map={sunBloom} />
+        </sprite>
+      </group>
+    </>
   );
 };
 
